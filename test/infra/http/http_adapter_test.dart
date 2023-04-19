367e57d3d1202a5a18b0f0e449d1e9ec59c016c5
Sir-kof <rodrigo_mdg_fernandes@outlook.com>
2023-04-19 15:39:44 -0300

    ensure HttpAdapter throws UnauthorizedError on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -81,7 +81,7 @@ void main() {
       expect(response, null);
     });
 
-    test('Should return data if post returns 400', () async {
+    test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400, body: '');
 
       final future = sut.request(url: url, method: 'post');
@@ -89,7 +89,7 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
-    test('Should return data if post returns 400', () async {
+    test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400);
 
       final future = sut.request(url: url, method: 'post');
@@ -97,7 +97,15 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
-    test('Should return data if post returns 500', () async {
+    test('Should return BadRequestError if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
 
       final future = sut.request(url: url, method: 'post');
