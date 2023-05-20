76b8c3bffe5b14415d9e580b6af4706c8669d493
Sir-kof <rodrigo_mdg_fernandes@outlook.com>
2023-05-20 17:19:00 -0300

    add LoginPage layout

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -4,9 +4,46 @@ import '../pages/pages.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final primatyColor = Color.fromRGBO(136, 14, 19, 1);
+    final primatyColorDark = Color.fromRGBO(96, 0, 39, 1);
+    final primatyColorLigth = Color.fromRGBO(188, 71, 123, 1);
+
     return MaterialApp(
       title: '4Dev',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primatyColor,
+        primaryColorDark: primatyColorDark,
+        primaryColorLight: primatyColorLigth,
+        accentColor: primatyColor,
+        backgroundColor: Colors.white,
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: primatyColorDark,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primatyColorLigth)
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primatyColor),
+          ),
+          alignLabelWithHint: true,
+        ),
+        buttonTheme: ButtonThemeData(
+          colorScheme: ColorScheme.light(primary: primatyColor),
+          buttonColor: primatyColor,
+          splashColor: primatyColorLigth,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20,),
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
       home: LoginPage(),
     );
   }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
 
   @override
@@ -7,38 +9,49 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Image(image: AssetImage('lib/ui/assets/logo.png'),),
-            ),
-            Text('Login'.toUpperCase()),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(Icons.email),
+            LoginHeader(),
+            HeadLine1(text: 'Login',),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColorLight,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      icon: Icon(Icons.email),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(
+                            Icons.lock,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
                     ),
-                    obscureText: true,
-                  ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text('Entrar'.toUpperCase()),
-                  ),
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.person),
-                    label: Text('Criar Conta'),
-                  ),
-                ],
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text('Entrar'.toUpperCase()),
+                    ),
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.person),
+                      label: Text('Criar Conta'),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
