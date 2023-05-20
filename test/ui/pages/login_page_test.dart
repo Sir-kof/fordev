1629afaef8374c19420a65775cb2987e6c4256af
Sir-kof <rodrigo_mdg_fernandes@outlook.com>
2023-05-20 20:37:18 -0300

    ensure LoginPage presents no error if password is valid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -71,7 +71,7 @@ void main() {
     expect(find.text('any error'), findsOneWidget);
   });
 
-  testWidgets('Should present error if email is invalid', (WidgetTester tester) async {
+  testWidgets('Should present error if email is valid', (WidgetTester tester) async {
     await loadPage(tester);
 
     emailErrorController.add(null);
@@ -83,7 +83,7 @@ void main() {
     );
   });
 
-  testWidgets('Should present error if email is invalid', (WidgetTester tester) async {
+  testWidgets('Should present error if email is valid', (WidgetTester tester) async {
     await loadPage(tester);
 
     emailErrorController.add('');
@@ -95,7 +95,6 @@ void main() {
     );
   });
 
-
   testWidgets('Should present error if password is invalid', (WidgetTester tester) async {
     await loadPage(tester);
 
@@ -103,5 +102,30 @@ void main() {
     await tester.pump();
 
     expect(find.text('any error'), findsOneWidget);
+  });
+
+
+  testWidgets('Should present error if password is valid', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add(null);
+    await tester.pump();
+
+    expect(
+      find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text)),
+      findsOneWidget,
+    );
+  });
+
+  testWidgets('Should present error if password is valid', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add('');
+    await tester.pump();
+
+    expect(
+      find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text)),
+      findsOneWidget,
+    );
   });
 }
